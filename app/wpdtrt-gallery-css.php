66ed9766d1d1c23589b7b2161d170ab435ab5353
Dan Smith <dev@dotherightthing.co.nz>
2017-09-13 13:33:32 +1200

    Load paver stylesheet before plugin styles;

--- app/wpdtrt-gallery-css.php
+++ app/wpdtrt-gallery-css.php
@@ -43,20 +43,23 @@ if ( !function_exists( 'wpdtrt_gallery_css_frontend' ) ) {
    */
   function wpdtrt_gallery_css_frontend() {
 
-    $frontend = 'wpdtrt_gallery_frontend';
+    $media = 'all';
 
-    wp_enqueue_style( $frontend,
-      WPDTRT_GALLERY_URL . 'css/wpdtrt-gallery.css',
-      array(),
-      WPDTRT_GALLERY_VERSION
-      //'all'
-    );
-
-    wp_enqueue_style( 'wpdtrt_gallery_paver',
+    wp_register_style( 'paver',
       WPDTRT_GALLERY_URL . 'vendor/bower_components/paver/dist/css/paver.min.css',
       array(),
-      WPDTRT_GALLERY_VERSION
-      //'all'
+      '1.3.3',
+      $media
+    );
+
+    wp_enqueue_style( 'wpdtrt_gallery_frontend',
+      WPDTRT_GALLERY_URL . 'css/wpdtrt-gallery.css',
+      array(
+        // registered dependencies:
+        'paver'
+      ),
+      WPDTRT_GALLERY_VERSION,
+      $media
     );
 
   }
