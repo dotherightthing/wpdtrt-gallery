663165d77a259b25243c3595c1169d3a079194a0
Dan Smith <dev@dotherightthing.co.nz>
2018-03-07 13:06:39 +1300

    Convert CSS to SCSS, add gulpfile to run PHP Lint and SCSS compiling

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,77 @@
 /**
- * gulpfile.js
- * Run tasks to aid the development process.
- * @example gulp default
+ * Gulp Task Runner
+ * Compile front-end resources
+ *
+ * Based on the gulpfile from WPDTRT_Plugin, which this plugin predates.
+ *
+ * @package     WPDTRT_Gallery
+ * @since       1.2.0
+ * @version     1.0.0
  */
-var gulp = require('gulp');
-var phplint = require('gulp-phplint');
 
-gulp.task('default', function () {
-  return gulp.src(['**/*.php'])
+/* global require */
+
+// dependencies
+var gulp = require('gulp');
+var autoprefixer = require('autoprefixer');
+var phplint = require('gulp-phplint');
+var postcss = require('gulp-postcss');
+var pxtorem = require('postcss-pxtorem');
+var sass = require('gulp-sass');
+
+var scssDir = './scss/*.scss';
+var cssDir = './css/';
+var phpDir = [
+  './**/*.php',
+  '!vendor/**/*',
+  '!node_modules/**/*'
+];
+
+// tasks
+
+gulp.task('scss', function () {
+
+  var processors = [
+      autoprefixer({
+        cascade: false
+      }),
+      pxtorem({
+        rootValue: 16,
+        unitPrecision: 5,
+        propList: [
+          'font',
+          'font-size',
+          'padding',
+          'padding-top',
+          'padding-right',
+          'padding-bottom',
+          'padding-left',
+          'margin',
+          'margin-top',
+          'margin-right',
+          'margin-bottom',
+          'margin-left',
+          'bottom',
+          'top',
+          'max-width'
+        ],
+        selectorBlackList: [],
+        replace: false,
+        mediaQuery: true,
+        minPixelValue: 0
+      })
+  ];
+
+  return gulp
+    .src(scssDir)
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(cssDir));
+});
+
+gulp.task('phplint', function () {
+  return gulp
+    .src(phpDir)
 
     // validate PHP
     // The linter ships with PHP
@@ -20,3 +84,14 @@ gulp.task('default', function () {
       }
     }));
 });
+
+gulp.task('watch', function () {
+  gulp.watch( scssDir, ['scss'] );
+});
+
+gulp.task( 'default', [
+    'phplint',
+    'scss',
+    'watch'
+  ]
+);
