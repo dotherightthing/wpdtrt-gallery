f1c7f4a128d71883f122f0566e3f982d90ca2b4a
Dan Smith <dev@dotherightthing.co.nz>
2018-05-07 16:36:36 +1200

    Pass flag to fix path to autoloader when loading test dependency (dotherightthing/wpdtrt-plugin#104)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,6 +23,10 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/wpdtrt-gallery.php';
+
+	if ( !defined( 'WPDTRT_CONTENTSECTIONS_TEST_DEPENDENCY' ) ) {
+		define( 'WPDTRT_CONTENTSECTIONS_TEST_DEPENDENCY', true );
+	}
 	require dirname( dirname( __FILE__ ) ) . '/vendor/dotherightthing/wpdtrt-contentsections/wpdtrt-contentsections.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
