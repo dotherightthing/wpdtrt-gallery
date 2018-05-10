74ad89e483f2c41183b6ee08fb25bb86b1d5e6a9
Dan Smith <dev@dotherightthing.co.nz>
2018-05-11 03:26:17 +1200

    Bump version

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@
  *    gulp dist --gulpfile ./vendor/dotherightthing/wpdtrt-plugin/gulpfile.js --cwd ./
  *
  * @package     WPDTRT_Gallery
- * @version 	1.7.9
+ * @version 	1.7.10
  * @since       0.7.0
  */
  
--- wpdtrt-gallery.php
+++ wpdtrt-gallery.php
@@ -3,7 +3,7 @@
  * Plugin Name:  DTRT Gallery
  * Plugin URI:   https://github.com/dotherightthing/wpdtrt-gallery
  * Description:  Gallery viewer which supports images, panoramas, maps, SoundCloud and Vimeo.
- * Version:      1.7.9
+ * Version:      1.7.10
  * Author:       Dan Smith
  * Author URI:   https://profiles.wordpress.org/dotherightthingnz
  * License:      GPLv2 or later
@@ -67,7 +67,7 @@ if( ! defined( 'WPDTRT_GALLERY_VERSION' ) ) {
  * @version   0.0.1
  * @since     0.7.0
  */
-  define( 'WPDTRT_GALLERY_VERSION', '1.7.9' );
+  define( 'WPDTRT_GALLERY_VERSION', '1.7.10' );
 }
 
 if( ! defined( 'WPDTRT_GALLERY_PATH' ) ) {
