4a8c9b4ec8e67cf767648f42f7d2b409aace010c
Dan Smith <dev@dotherightthing.co.nz>
2021-01-29 11:41:50 +1300

    Update wpdtrt-plugin-boilerplate from 1.7.14 to 1.7.15

--- src/class-wpdtrt-gallery-plugin.php
+++ src/class-wpdtrt-gallery-plugin.php
@@ -17,7 +17,7 @@
  * Since:
  *   0.8.13 - DTRT WordPress Plugin Boilerplate Generator
  */
-class WPDTRT_Gallery_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_7_14\Plugin {
+class WPDTRT_Gallery_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_7_15\Plugin {
 
 	/**
 	 * Constructor: __construct
